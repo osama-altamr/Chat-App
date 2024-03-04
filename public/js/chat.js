4e4a942190787a74ad17c1df7fd7c64fe1ec55f6
unknown <oaltamr18@gmail.com>
2024-03-05 00:18:58 +0300

    Implement broadcast.emit for real-time messaging

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,8 +4,24 @@ socket.on("message", (message) => {
   console.log(message);
 });
 
+
+
 document.querySelector("#message-form").addEventListener("submit", (e) => {
   e.preventDefault();
-  const message = document.querySelector("input").value;
-  socket.emit("sendMessage",message);
+  const message = e.target.elements.message.value;
+  socket.emit("sendMessage", message);
+});
+
+document.querySelector("#send-location").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser");
+  }
+  navigator.geolocation.getCurrentPosition((position) => {
+    const coords = {
+      lat: position.coords.latitude,
+      long: position.coords.longitude,
+    };
+
+    socket.emit("location", coords);
+  });
 });
--- src/index.js
+++ src/index.js
@@ -12,14 +12,34 @@ const port = 3000 || process.env.PORT;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
-let count = 0;
+
 io.on("connection", (socket) => {
   socket.emit("message", "Welcome!");
+  socket.broadcast.emit("message", "A new user has joined!");
   socket.on("sendMessage", (message) => {
     console.log(message);
     io.emit("message", message);
+  });
+
+  socket.on("location", (coords) => {
+    console.log(coords);
+    io.emit("message", `https://www.google.com/maps?q=${coords.lat}, ${coords.long}`);
+  });
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left!");
   });
 });
 server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
+
+/* 
+-When we use socket.emit we are emitting the event to a particular connection.
+-If i used io.emit here every time a new client joined all clients would get the count.
+-I want to emit it to every single connection. So I used io.emit 
+-When we broadcast an event :We send it to everybody except the current client.
+-Event Acknowledgements in socketio ,example use case sending a location or a message the client sends the 
+the msg or the loc to the server but it's never quite sure the server recevied it and actually did 
+something,with and ack the client would get notified that the message or the location was indeed delivered
+successfully  
+*/
