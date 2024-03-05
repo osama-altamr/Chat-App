152f6db097ec13aa3e8c1185f82afc41e4231736
unknown <oaltamr18@gmail.com>
2024-03-06 01:59:49 +0300

    Added functionality to create and join rooms using Socket.IO

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,27 +1,77 @@
 const socket = io();
+// Elements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+
+const $sendLocationButton = document.querySelector("#send-location");
+const $messages = document.querySelector("#messages");
+
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const {username, room } = Qs.parse(location.search, {ignoreQueryPrefix:true})
+
+const locationMessageTemplate = document.querySelector(
+  "#location-message-template"
+).innerHTML;
 
 socket.on("message", (message) => {
   console.log(message);
+  const html = Mustache.render(messageTemplate, {
+    message: message.text,
+    createdAt: moment(message.createdAt).format("h:mm a"),
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
 });
 
+socket.on("locationMessage", (message) => {
+  console.log(message);
+  const html = Mustache.render(locationMessageTemplate, {
+    url: message.url,
+    createdAt: moment(message.createdAt).format("h:mm a"),
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
 
-
-document.querySelector("#message-form").addEventListener("submit", (e) => {
+$messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  // disable
+  $messageFormButton.setAttribute("disabled", "disabled");
   const message = e.target.elements.message.value;
-  socket.emit("sendMessage", message);
+
+  socket.emit("sendMessage", message, (err) => {
+    // enable
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Message delivered successfully!");
+    // console.log(deleverd)
+  });
 });
 
-document.querySelector("#send-location").addEventListener("click", () => {
+$sendLocationButton.addEventListener("click", () => {
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser");
   }
+  $sendLocationButton.setAttribute("disabled", "disabled");
   navigator.geolocation.getCurrentPosition((position) => {
     const coords = {
       lat: position.coords.latitude,
       long: position.coords.longitude,
     };
 
-    socket.emit("location", coords);
+    socket.emit("location", coords, (err) => {
+      $sendLocationButton.removeAttribute("disabled");
+      if (err) {
+        return console.log(err);
+      }
+
+      console.log("Location shared!");
+    });
   });
 });
+
+socket.emit('join',{username,room})
--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ const http = require("http");
 const express = require("express");
 const path = require("path");
 const socketio = require("socket.io");
+var Filter = require("bad-words");
+const {
+  generateMessage,
+  generateLocationMessage,
+} = require("./utils/messages");
 
 const app = express();
 const server = http.createServer(app);
@@ -14,19 +19,41 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
 
 io.on("connection", (socket) => {
-  socket.emit("message", "Welcome!");
-  socket.broadcast.emit("message", "A new user has joined!");
-  socket.on("sendMessage", (message) => {
-    console.log(message);
-    io.emit("message", message);
+
+
+  socket.on("join", ({ username, room }, callback) => {
+    socket.join(room);
+    socket.emit("message", generateMessage("Welcome!"));
+    socket.broadcast.to(room).emit("message", generateMessage(`${username} has joined!`));
   });
 
-  socket.on("location", (coords) => {
-    console.log(coords);
-    io.emit("message", `https://www.google.com/maps?q=${coords.lat}, ${coords.long}`);
+
+
+  socket.on("sendMessage", (message, callback) => {
+    const filter = new Filter();
+    if (filter.isProfane(message)) {
+      return callback("Profanity is not allowed!");
+    }
+    io.emit("message", generateMessage(message));
+
+    // callback();
+    callback();
   });
+
+  socket.on("location", (coords, callback) => {
+    console.log(coords);
+    io.emit(
+      "locationMessage",
+      generateLocationMessage(
+        `https://www.google.com/maps?q=${coords.lat},${coords.long}`
+      )
+    );
+    callback();
+  });
+
+
   socket.on("disconnect", () => {
-    io.emit("message", "A user has left!");
+    io.emit("message", generateMessage("A user has left!"));
   });
 });
 server.listen(port, () => {
@@ -42,4 +69,9 @@ server.listen(port, () => {
 the msg or the loc to the server but it's never quite sure the server recevied it and actually did 
 something,with and ack the client would get notified that the message or the location was indeed delivered
 successfully  
+-socket.join allows us to join a given chat room we pass to it the name of the room
+socket.join we have two new setups we'll be using for emitting messages 
+ 1- io.to.emit : emit an event to everybody in a specific room  without sending it to people in other rooms
+ 2- socket.broadcast.to.emit : send an event to everyone in a specific room except the current client 
+
 */
